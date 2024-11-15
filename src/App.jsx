4da207adf1a51ca7fe1767ba05eb20eb47078e33
feryokta16 <110111575+feryokta16@users.noreply.github.com>
2024-11-15 21:18:30 +0700

    Feat Create UI For Login Page And Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/LayoutComponent";
@@ -104,3 +105,26 @@ const App = () => {
 
 export default App;
 
+=======
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/LayoutComponent";
+import LoginComponent from "./components/LoginComponent";
+import RegisterComponent from "./components/RegisterComponent";
+
+const App = () => {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/register" element={<RegisterComponent />} />
+        </Routes>
+    </Router>
+  );
+};
+  );
+};
+
+export default App;
+
+>>>>>>> bd758ea (Feat Create UI For Login Page And Register Page)
---
+++ src/components/LoginComponent.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
+import logo from "../assets/companylogo.png";
+import { Link } from "react-router-dom";
+
+const LoginComponent = () => {
+  const [passwordType, setPasswordType] = useState("password");
+
+  const togglePasswordVisibility = () => {
+    setPasswordType(passwordType === "password" ? "text" : "password");
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+        <div className="flex justify-center mb-6">
+          <img src={logo} alt="company logo" />
+        </div>
+        <h2 className="text-center text-2xl font-semibold mb-2 text-primary">
+          Logo Ipsum
+        </h2>
+        <p className="text-center text-sm text-secondary mb-6">
+          Better Insights For Business Growth
+        </p>
+        <form>
+          <div className="mb-4">
+            <label className="block text-secondary text-sm font-sem">
+              Email
+            </label>
+            <input
+              type="text"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
+              id="email"
+              value="contohnya"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-secondary text-sm font-sem">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                type={passwordType}
+                id="password"
+                value="feryokta"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"
+              >
+                {passwordType === "password" ? <FaEye /> : <FaEyeSlash />}
+              </button>
+            </div>
+            <div className="flex  items-center justify-center my-5">
+              <button
+                type="submit"
+                className="bg-primary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                SIGN IN
+              </button>
+            </div>
+          </div>
+          <div className="text-center text-secondary">
+            <p>
+              Dont Have Account ?{" "}
+              <Link to="/register">
+                <span className="text-primary">Daftar disini</span>
+              </Link>{" "}
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginComponent;
