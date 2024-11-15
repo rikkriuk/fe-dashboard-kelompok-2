2bf5333d8a02fe5168019e78480dd571939eb13a
feryokta16 <110111575+feryokta16@users.noreply.github.com>
2024-11-15 21:18:30 +0700

    Feat Create UI For Login Page And Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/LayoutComponent";
+import LoginComponent from "./components/LoginComponent";
+import RegisterComponent from "./components/RegisterComponent";
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={"Dashboard"} />
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/register" element={<RegisterComponent />} />
         </Routes>
       </Layout>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
