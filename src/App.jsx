821ee9fce36c7d05e4737545f23493d46f5697b0
feryokta16 <110111575+feryokta16@users.noreply.github.com>
2024-11-16 18:43:11 +0700

    feat About and teams and What we do

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/LayoutComponent";
 import "flowbite";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="dashboard" element={<h1>Dashboard</h1>} />
-          <Route path="dashboard/testimonial" element={<h1>Testimonial</h1>} />
-        </Route>
-      </Routes>
+      <Routes></Routes>
     </Router>
   );
 };
---
+++ src/components/AboutComponent.jsx
@@ -0,0 +1,157 @@
+import React from "react";
+import {
+  FaHome,
+  FaAngleRight,
+  FaSearch,
+  FaPlus,
+  FaPencilAlt,
+} from "react-icons/fa";
+
+const AboutComponent = () => {
+  return (
+    <div>
+      <div className="container mx-auto px-52 pt-10">
+        <nav className="flex mb-3" aria-label="Breadcrumb">
+          <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li className="inline-flex items-center">
+              <a
+                href="/"
+                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+              >
+                <FaHome className="mr-2" />
+                Home
+              </a>
+            </li>
+
+            <li aria-current="page">
+              <div className="flex items-center">
+                <FaAngleRight />
+                <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                  About Us
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+
+        <div className="relative overflow-x-auto p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex items-center flex-column flex-wrap md:flex-row justify-between mb-4">
+            <h5 className="text-xl font-bold text-gray-600 dark:text-white">
+              About List
+            </h5>
+
+            <div className="flex items-center space-x-4">
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                  <FaSearch />
+                </div>
+                <input
+                  type="text"
+                  id="table-search"
+                  className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-50 h-8 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search"
+                />
+              </div>
+              <a
+                type="button"
+                href="testimonial/add"
+                className="px-3 py-2 text-xs font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 whitespace-nowrap inline-flex items-center"
+              >
+                <FaPlus className="mr-2" /> Add Data
+              </a>
+            </div>
+          </div>
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 overflow-x-auto">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  No
+                </th>
+                <th scope="col" className="px-6 py-3 max-w-xs">
+                  Title
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Message
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Image
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td className="px-6 py-4">1</td>
+                <td className="px-6 py-4">CEO Academy</td>
+                <td className="px-6 py-4">pelayanan yang sangat mantap</td>
+                <td className="px-6 py-4">aboutUs.png</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <a
+                    href="testimonial/edit/1"
+                    className="inline-flex items-center mx-3 font-medium text-primary dark:text-blue-500 me-3 hover:text-red-700 dark:hover:text-blue-400"
+                  >
+                    <FaPencilAlt />
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <nav
+            className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
+            aria-label="Table navigation"
+          >
+            <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+              Showing{" "}
+              <span className="font-semibold text-gray-900 dark:text-white">
+                1-10
+              </span>{" "}
+              of{" "}
+              <span className="font-semibold text-gray-900 dark:text-white">
+                1000
+              </span>
+            </span>
+            <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Previous
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  1
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  2
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                >
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutComponent;
