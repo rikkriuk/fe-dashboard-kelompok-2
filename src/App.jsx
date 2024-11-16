e00275185ce910a5035bbdfd3d46661eb6f8428e
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-11-16 12:16:59 +0700

    feat : create ui table, form and detail testimonial component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/LayoutComponent";
 import PortofolioContainer from "./containers/PortofolioContainer";
+import TestimonialContainer from "./containers/TestimonialContainer";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={"Dashboard"} />
           <Route path="/*" element={"tes"} />
+          <Route path="/testimonial" element={<TestimonialContainer />} />
           <Route path="/portofolio" element={<PortofolioContainer />} />
         </Routes>
       </Layout>
