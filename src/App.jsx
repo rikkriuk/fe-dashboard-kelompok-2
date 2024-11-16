eda7837a3574063a9fbf707ba5c51fa1d7d0cca1
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-11-16 13:53:29 +0700

    fix : delete testimonial detail and add logic page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/LayoutComponent";
-import LoginComponent from "./components/LoginComponent";
-import RegisterComponent from "./components/RegisterComponent";
-import SubscribeEmailComponent from "./components/SubscribeEmailComponent";
-import ContactComponent from "./components/ContactComponent";
-import AboutUsComponent from "./components/AboutUsComponent";
-import WhatWeDoComponent from "./components/WhatWeDoComponent";
+import "flowbite";
+import PortfolioContainer from "./containers/PortfolioContainer";
+import TestimonialContainer from "./containers/TestimonialContainer";
 
 const App = () => {
   return (
     <Router>
-        <Routes>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="dashboard" element={<h1>Dashboard</h1>} />
+          <Route
+            path="dashboard/testimonial"
+            element={<TestimonialContainer />}
+          />
+          <Route
+            path="dashboard/testimonial/add"
+            element={<TestimonialContainer />}
+          />
+          <Route
+            path="dashboard/testimonial/edit/:id"
+            element={<TestimonialContainer />}
+          />
+          <Route path="dashboard/portfolio" element={<PortfolioContainer />} />
+          <Route
+            path="dashboard/portfolio/add"
+            element={<PortfolioContainer />}
+          />
+          <Route
+            path="dashboard/portfolio/edit/:id"
+            element={<PortfolioContainer />}
+          />
+        </Route>
+      </Routes>
     </Router>
   );
 };
