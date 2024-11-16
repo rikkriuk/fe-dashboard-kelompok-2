7aac74c7f6ee150904e2521f1634166f5ca76ce3
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-11-16 09:29:12 +0700

    feat : create list portofolio

---
+++ src/containers/PortofolioContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TablePortofolioComponent from "../components/Portofolio/TablePortofolioComponent";
+
+const PortofolioContainer = () => {
+  return (
+    <div>
+      <TablePortofolioComponent />
+    </div>
+  );
+};
+
+export default PortofolioContainer;
