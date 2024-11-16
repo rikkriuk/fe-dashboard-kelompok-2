7a13968060a29f59742f661cda1f54657bf6b5ba
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-11-16 09:29:12 +0700

    feat : create list portofolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/LayoutComponent";
+import PortofolioContainer from "./containers/PortofolioContainer";
 
 const App = () => {
   return (
@@ -8,10 +9,12 @@ const App = () => {
       <Layout>
         <Routes>
           <Route path="/" element={"Dashboard"} />
+          <Route path="/*" element={"tes"} />
+          <Route path="/portofolio" element={<PortofolioContainer />} />
         </Routes>
       </Layout>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
