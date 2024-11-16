dbc4f5504fb38c62796dfd89a3f9fde9f8714acd
feryokta16 <110111575+feryokta16@users.noreply.github.com>
2024-11-16 12:43:21 +0700

    feat create Contact feature and Subscribe

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/LayoutComponent";
 import LoginComponent from "./components/LoginComponent";
 import RegisterComponent from "./components/RegisterComponent";
+import SubscribeEmailComponent from "./components/SubscribeEmailComponent";
+import ContactComponent from "./components/ContactComponent";
+import AboutUsComponent from "./components/AboutUsComponent";
+import WhatWeDoComponent from "./components/WhatWeDoComponent";
 
 const App = () => {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<LoginComponent />} />
-          <Route path="/register" element={<RegisterComponent />} />
         </Routes>
     </Router>
   );
