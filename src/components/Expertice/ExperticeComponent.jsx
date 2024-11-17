3fdcdeb5384e73c7fe9c37d621be73a3c84fae01
Devran <devranmalik82@gmail.com>
2024-11-17 14:46:23 +0700

    feat: create Expertice table items

---
+++ src/components/Expertice/ExperticeComponent.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import {
+  FaHome,
+  FaPencilAlt,
+  FaTrashAlt,
+  FaAngleRight,
+  FaPlus,
+  FaSearch,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const ExpeticeComponent = ({expertice}) => {
+  return (
+    <div className="container mx-auto px-10 pt-10">
+      <nav className="flex mb-3" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+          <li className="inline-flex items-center">
+            <Link
+              to="/"
+              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+            >
+              <FaHome className="mr-2" />
+              Home
+            </Link>
+          </li>
+
+          <li aria-current="page">
+            <div className="flex items-center">
+              <FaAngleRight />
+              <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                Expertice
+              </span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+
+      <div className="relative overflow-x-auto p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex items-center flex-column flex-wrap md:flex-row justify-between mb-4">
+          <h5 className="text-xl font-bold text-gray-600 dark:text-white">
+            Expertice List
+          </h5>
+
+          <div className="flex items-center space-x-4">
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                <FaSearch />
+              </div>
+              <input
+                type="text"
+                id="table-search"
+                className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-50 h-8 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search"
+              />
+            </div>
+            <Link to="/expertice/add">
+              <button
+                type="button"
+                className="px-3 py-2 text-xs font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 whitespace-nowrap inline-flex items-center"
+              >
+                <FaPlus className="mr-2" /> Add Data
+              </button>
+            </Link>
+          </div>
+        </div>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 overflow-x-auto">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                No
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3 max-w-xs">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Icon
+              </th>
+              <th scope="col" className="px-6 py-3">
+                action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {expertice.map((item, index) => (
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={index}>
+                <td className="px-6 py-4">{index + 1}</td>
+                <td className="px-6 py-4">{item.title}</td>
+                <td className="px-6 py-4 max-w-xs">{item.desc}</td>
+                <td className="px-6 py-4">
+                  <img src={item.icon} className="w-20 h-20" alt={item.title} />
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <Link to={`/expertice/edit/${item.id}`}>
+                    <button
+                      className="inline-flex items-center font-medium text-primary dark:text-blue-500 me-3 hover:text-red-700 dark:hover:text-blue-400"
+                    >
+                      <FaPencilAlt />
+                    </button>
+                  </Link>
+                  <Link to={`/expertice/delete/${item.id}`}>
+                    <button
+                      className="inline-flex items-center font-medium text-primary dark:text-blue-500 hover:text-red-700 dark:hover:text-blue-400"
+                    >
+                      <FaTrashAlt />
+                    </button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ExpeticeComponent;
