5c8e23961cf066696eebe986d293445e2823ba38
Devran <devranmalik82@gmail.com>
2024-11-17 22:32:35 +0700

    feat:Create Expertice

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import SubscribeEmailComponent from "./components/SubscribeEmail/SubscribeEmailC
 import TeamsContainer from "./containers/TeamsContainer";
 import WhatWeDoContainer from "./containers/WhatWeDoContainer";
 import AboutUsContainer from "./containers/AboutUsContainer";
+import ExperticeContainer from "./containers/ExperticeContainer";
 
 const App = () => {
   return (
@@ -210,6 +211,15 @@ const App = () => {
               element={
                 <ProtectedRoute>
                   <SubscribeEmailComponent />
+                </ProtectedRoute>
+              }
+            />
+            {/* Expertice route */}
+            <Route
+              path="dashboard/expertise"
+              element={
+                <ProtectedRoute>
+                  <ExperticeContainer/>
                 </ProtectedRoute>
               }
             />
