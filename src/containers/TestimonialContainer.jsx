d82a94a16ea7d33cb53cf6a7989c89814669d41e
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-11-17 13:21:13 +0700

    feat : add CRUD testimonial page and add loading spinner

--- src/containers/TestimonialContainer.jsx
+++ src/containers/TestimonialContainer.jsx
@@ -1,24 +1,179 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TableTestimonialComponent from "../components/Testimonial/TableTestimonialComponent";
 import FormTestimonialComponent from "../components/Testimonial/FormTestimonialComponent";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import useForm2 from "../hooks/useForm2";
+import {
+  getTestimonial,
+  addTestimonial,
+  editTestimonial,
+  deleteTestimonial,
+} from "../utils/api";
+import {
+  showConfirmationAlert,
+  showErrorAlert,
+  showSuccessAlert,
+} from "../utils/alert";
+
 const TestimonialContainer = () => {
-  const testimonial = {
-    name: "Deni Irawan",
-    title: "CEO Academy",
-    message: "pelayanan yang sangat mantap",
-    date: "12/12/2025",
-    image: "https://via.placeholder.com/300",
-  };
+  const { id } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [testimonial, setTestimonial] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+
   const isAdd = location.pathname.includes("/add");
   const isEdit = location.pathname.includes("/edit");
+
+  const [form, setForm, handleChange] = useForm2({
+    name: "",
+    message: "",
+    date: "",
+    title: "",
+    image: "",
+  });
+
+  useEffect(() => {
+    fetchData();
+  }, [refresh]);
+
+  const handleRefresh = () => {
+    setRefresh((prev) => !prev);
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await getTestimonial();
+      console.log(res.data.data);
+      setTestimonial(res.data.data);
+    } catch (err) {
+      console.log(err);
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const navigationForm = (location, data) => {
+    if (location == "edit") {
+      setForm(data);
+      navigate(`edit/${data.id}`);
+    } else if (location == "add") {
+      setForm({
+        name: "",
+        message: "",
+        date: "",
+        title: "",
+        image: "",
+      });
+      navigate("add");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", form.name);
+    formData.append("message", form.message);
+    formData.append("date", form.date);
+    formData.append("title", form.title);
+    formData.append("image", form.image);
+
+    setLoading(true);
+    try {
+      if (isEdit) {
+        const response = await editTestimonial(id, formData);
+        console.log("Success:", response.data);
+        showSuccessAlert("Data berhasil diedit !");
+      } else {
+        const response = await addTestimonial(formData);
+        console.log("Success:", response.data);
+        showSuccessAlert("Data berhasil ditambahkan !");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      showErrorAlert("Terjadi kesalahan data!");
+    } finally {
+      setLoading(false);
+      handleRefresh();
+      navigate("/dashboard/testimonial");
+    }
+  };
+
+  const handleDelete = async (idtestimoni) => {
+    const confirmation = await showConfirmationAlert(
+      "Konfirmasi Hapus",
+      "Apakah Anda yakin ingin menghapus data ini?"
+    );
+
+    if (confirmation.isConfirmed) {
+      try {
+        setLoading(true);
+        const response = await deleteTestimonial(idtestimoni);
+        console.log("Success:", response.data);
+        showSuccessAlert("Data berhasil dihapus!");
+      } catch (error) {
+        console.error("Error:", error);
+        showErrorAlert("Terjadi kesalahan saat menghapus data!");
+      } finally {
+        setLoading(false);
+        handleRefresh();
+        navigate("/dashboard/testimonial");
+      }
+    }
+  };
+
+  if (loading) {
+    return (
+      <div class="text-center mt-96">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            class="inline w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-primary"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <>
       {!isAdd && !isEdit && (
-        <TableTestimonialComponent testimonial={testimonial} />
+        <TableTestimonialComponent
+          testimonial={testimonial}
+          navigationForm={navigationForm}
+          handleDelete={handleDelete}
+        />
       )}
 
-      {(isAdd || isEdit) && <FormTestimonialComponent isEdit={isEdit} />}
+      {(isAdd || isEdit) && (
+        <FormTestimonialComponent
+          isEdit={isEdit}
+          form={form}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+      )}
     </>
   );
 };
